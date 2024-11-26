eaaf9ae33b3edc84f7bf53b0e2e656f84430aada
Nicolas Pasquale <139779304+HelpiaNicoP@users.noreply.github.com>
2024-11-26 17:43:59 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,79 +1,66 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { UserConfig } from 'vite'
 
-export default defineConfig({
+const config: UserConfig = {
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@assets': path.resolve(__dirname, './src/assets'),
     }
+  },
+  optimizeDeps: {
+    include: [
+      '@mui/material',
+      '@mui/icons-material',
+      '@emotion/react',
+      '@emotion/styled'
+    ]
   },
   build: {
     outDir: '../gents-back/static/frontend',
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            if (id.includes('@mui')) {
-              return 'vendor-mui'
-            }
-            if (id.includes('react/')) {
-              return 'vendor-react'
-            }
-            if (id.includes('react-dom')) {
-              return 'vendor-react-dom'
-            }
-            const match = id.match(/node_modules\/([^/]+)/)
-            if (match && match[1]) {
-              return `vendor-${match[1]}`
-            }
-          }
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          mui: ['@mui/material', '@mui/icons-material'],
+          styles: ['@emotion/react', '@emotion/styled']
         },
         assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.')
-          const ext = info[info.length - 1]
-          if (/\.(woff|woff2|eot|ttf|otf)$/i.test(assetInfo.name)) {
-            return `assets/fonts/[name][extname]`
+          const type = assetInfo.name.split('.').pop()
+          const mapping = {
+            woff: 'fonts',
+            woff2: 'fonts',
+            ttf: 'fonts',
+            css: 'css',
           }
-          if (/\.css$/i.test(assetInfo.name)) {
-            return `assets/css/[name]-[hash][extname]`
-          }
-          if (['ico', 'json'].includes(ext)) {
-            return `[name].[ext]`
-          }
-          return `assets/[ext]/[name]-[hash][extname]`
+          const dir = mapping[type] || type
+          return `assets/${dir}/[name]-[hash][extname]`
         },
-        chunkFileNames: 'assets/js/[name].[hash].js',
-        entryFileNames: 'assets/js/[name].[hash].js',
-        format: 'es',
-      },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      }
     },
     manifest: true,
     assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'esbuild',
-    target: 'es2015',
-    modulePreload: {
-      polyfill: true
-    }
+    chunkSizeWarningLimit: 1500,
   },
   server: {
     proxy: {
       '/api': {
-        target: 'https://www.gentsbuilder.com',
+        target: process.env.VITE_API_URL || 'https://www.gentsbuilder.com',
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     },
     host: true,
-    port: 3000,
+    port: Number(process.env.PORT) || 3000,
+    cors: true
   },
   base: '/',
-})
+}
+
+export default defineConfig(config)
